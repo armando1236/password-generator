872260d65e674885b673a70e8c6cf1bbad083921
amjr86-DBxjRd <amjr86@outlook.com>
2022-05-31 19:43:26 -0700

    possible var

--- script.js
+++ script.js
@@ -3,7 +3,12 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+var possibleChars = []
+var passwordArray = []
+var possSpecChars = ['?', '.', ',', '!', '@', '#', '$', '%', '^', '&', '*']
+var possLowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var possUpperCase = ['A', 'B','C', 'D', 'E', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var possNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
